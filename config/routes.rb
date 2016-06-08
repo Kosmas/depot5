9c951ebdb7c9eac5d445c7a9aa5e21cbf09306f0
Kosmas Chatzimichalis <kosmas@mach7x.com>
2016-06-08 12:14:37 +0100

    C1 - Creating the catalog listing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'store/index'
+
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
+  root 'store#index', as: 'store'
 end
