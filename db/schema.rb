c92049e82ed1f8513d3b5e0eb1ef9a72dcd458ca
Kosmas Chatzimichalis <kosmas@mach7x.com>
2016-06-13 10:21:01 +0100

    E1 - Creating a smarter cart

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160610105123) do
+ActiveRecord::Schema.define(version: 20160613085655) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -21,8 +21,9 @@ ActiveRecord::Schema.define(version: 20160610105123) do
   create_table "line_items", force: :cascade do |t|
     t.integer  "product_id"
     t.integer  "cart_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "quantity",   default: 1
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
     t.index ["product_id"], name: "index_line_items_on_product_id"
   end
