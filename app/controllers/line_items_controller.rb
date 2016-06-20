f848c449ccfcbbe1398c783501f57f462dd75f60
Kosmas Chatzimichalis <kosmas@mach7x.com>
2016-06-20 15:44:24 +0100

    E2 - Handling Errors

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -65,13 +65,14 @@ class LineItemsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_line_item
-      @line_item = LineItem.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_line_item
+    @line_item = LineItem.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def line_item_params
+    params.require(:line_item).permit(:product_id)
+  end
 end
